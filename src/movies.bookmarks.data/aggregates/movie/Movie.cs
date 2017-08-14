3726720c227bbe6cf777368e3161fbe463d21e4c
Thiago Simionato <tgo_s@live.com>
2017-08-14 09:28:33 -0300

    Correçao do arquivo de config

--- src/movies.bookmarks.data/aggregates/movie/Movie.cs
+++ src/movies.bookmarks.data/aggregates/movie/Movie.cs
@@ -15,16 +15,34 @@ namespace movies.bookmarks.domain.aggregates
 
         [JsonProperty("genre_ids")]
         public ICollection<int> GenreIds { get; set; }
+        
         public int Id { get; set; }
+
+        [JsonProperty("original_language")]
         public string OriginalLanguage { get; set; }
+
+        [JsonProperty("original_title")]
         public string OriginalTitle { get; set; }
+
         public string Overview { get; set; }
+
+        [JsonProperty("release_date")]
         public DateTime ReleaseDate { get; set; }
+
+        [JsonProperty("poster_path")]
         public string PosterPath { get; set; }
+
         public double Popularity { get; set; }
+
         public string Title { get; set; }
+
+        [JsonProperty("video")]
         public bool HasVideo { get; set; }
+
+        [JsonProperty("vote_average")]
         public double VoteAverage { get; set; }
+
+        [JsonProperty("vote_count")]
         public int VoteCount { get; set; }
     }
 }
